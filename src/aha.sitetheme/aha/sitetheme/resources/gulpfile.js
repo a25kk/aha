dc613dbae76478368fcb3c81491d5ab2a8116986
Christoph Boehner <cb@vorwaerts-werbung.de>
2015-07-31 09:19:45 +0200

    Add several enhancements to the theme setup
    - initial gulp integration
    - rebuild after cleanup

---
+++ src/aha.sitetheme/aha/sitetheme/resources/gulpfile.js
@@ -0,0 +1,102 @@
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
+import del from 'del';
+import {stream as wiredep} from 'wiredep';
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+var cp          = require('child_process');
+
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+       baseDir: "./"
+    }
+  });
+});
+
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+gulp.task('images', function(){
+  gulp.src('src/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images/'));
+});
+
+// gulp.task('styles', function(){
+//   gulp.src(['sass/main.scss'])
+//     .pipe(plumber({
+//       errorHandler: function (error) {
+//         console.log(error.message);
+//         this.emit('end');
+//     }}))
+//     .pipe(sass({
+//       includePaths: ['bower_components'],
+//       loadPath: 'bower_components'
+//     }))
+//     .pipe(autoprefixer('last 2 versions'))
+//     .pipe(gulp.dest('dist/styles/'))
+//     .pipe(rename({suffix: '.min'}))
+//     .pipe(minifycss())
+//     .pipe(gulp.dest('dist/styles/'))
+//     .pipe(browserSync.reload({stream:true}))
+// });
+
+gulp.task('styles', () =>  {
+  return gulp.src('sass/main.scss')
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass.sync({
+      outputStyle: 'expanded',
+      precision: 10,
+      includePaths: ['bower_components'],
+      loadPath: 'bower_components'
+    }).on('error', $.sass.logError))
+    .pipe($.autoprefixer({browsers: ['last 1 version']}))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/styles/'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('scripts', function(){
+  return gulp.src('src/scripts/**/*.js')
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/scripts/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts/'))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('default', ['browser-sync'], function(){
+  gulp.watch("sass/**/*.scss", ['styles']);
+  gulp.watch("src/scripts/**/*.js", ['scripts']);
+  gulp.watch("*.html", ['bs-reload']);
+});
