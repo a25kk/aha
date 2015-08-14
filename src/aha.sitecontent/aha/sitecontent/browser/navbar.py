8add78e09acd8db543f9c312d2431141881bb79d
Christoph Boehner <cboehner@team23.de>
2015-08-14 18:13:28 +0200

    Add navbar component

---
+++ src/aha.sitecontent/aha/sitecontent/browser/navbar.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""Module providing custom navbar"""
+from Products.Five.browser import BrowserView
+from Products.ZCatalog.interfaces import ICatalogBrain
+from plone import api
+from plone.app.contentlisting.interfaces import IContentListingObject
+from zope.component import getMultiAdapter
+
+
+class NavBarView(BrowserView):
+    """ General purpose frontpage view """
+
+    def __call__(self):
+        return self.render()
+
+    def render(self):
+        return self.index()
+
+    def selectedTabs(self, default_tab='index_html', portal_tabs=()):
+        plone_url = api.portal.get().absolute_url()
+        plone_url_len = len(plone_url)
+        request = self.request
+        parent_request = request['PARENT_REQUEST']
+        valid_actions = []
+        url = parent_request['URL']
+        path = url[plone_url_len:]
+        for action in portal_tabs:
+            if not action['url'].startswith(plone_url):
+                continue
+            action_path = action['url'][plone_url_len:]
+            if not action_path.startswith('/'):
+                action_path = '/' + action_path
+            if path.startswith(action_path + '/') or path == action_path:
+                valid_actions.append((len(action_path), action['id']))
+        valid_actions.sort()
+        if valid_actions:
+            return {'portal': valid_actions[-1][1]}
+        return {'portal': default_tab}
+
+    def section(self):
+        portal = api.portal.get()
+        portal_tabs_view = getMultiAdapter((self.context, self.request),
+                                           name='portal_tabs_view')
+        portal_tabs = portal_tabs_view.topLevelTabs()
+        # selectedTabs = self.context.restrictedTraverse('selectedTabs')
+        selected_tabs = self.selectedTabs(portal_tabs=portal_tabs)
+        selected_section =  selected_tabs['portal']
+        if selected_section != 'index_html':
+            section = portal[selected_section]
+            return section.getId()
+        return portal.getId()
