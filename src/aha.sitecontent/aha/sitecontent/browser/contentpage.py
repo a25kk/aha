06f11069e7be4a062164772b104bd3a15945dbcb
Christoph Boehner <cboehner@team23.de>
2015-08-17 18:00:10 +0200

    Update content page view

--- src/aha.sitecontent/aha/sitecontent/browser/contentpage.py
+++ src/aha.sitecontent/aha/sitecontent/browser/contentpage.py
@@ -3,7 +3,9 @@
 import math
 from Acquisition import aq_inner
 from Products.Five.browser import BrowserView
+from Products.ZCatalog.interfaces import ICatalogBrain
 from plone import api
+from plone.app.contentlisting.interfaces import IContentListingObject
 from zope.component import getMultiAdapter
 from aha.sitecontent.contentpage import IContentPage
 
@@ -106,35 +108,46 @@ class ContentPageView(BrowserView):
         template = context.restrictedTraverse('@@gallery-view')()
         return template
 
-    def image_data(self):
+    def image_tag(self, item):
         data = {}
         sizes = ['small', 'medium', 'large']
         idx = 0
         for size in sizes:
             idx += 0
-            img = self._get_scaled_img(size)
+            img = self._get_scaled_img(item, size)
             data[size] = '{0} {1}w'.format(img['url'], img['width'])
         return data
 
-    def _get_scaled_img(self, size):
-        context = aq_inner(self.context)
-        scales = getMultiAdapter((context, self.request), name='images')
-        if size == 'small':
-            scale = scales.scale('image', width=300, height=300)
-        if size == 'medium':
-            scale = scales.scale('image', width=600, height=600)
+    def _get_scaled_img(self, item, size):
+        if (
+            ICatalogBrain.providedBy(item) or
+            IContentListingObject.providedBy(item)
+        ):
+            obj = item.getObject()
         else:
-            scale = scales.scale('image', width=900, height=900)
-        item = {}
-        if scale is not None:
-            item['url'] = scale.url
-            item['width'] = scale.width
-            item['height'] = scale.height
+            obj = item
+        info = {}
+        if hasattr(obj, 'image'):
+            scales = getMultiAdapter((obj, self.request), name='images')
+            if size == 'small':
+                scale = scales.scale('image', width=300, height=300)
+            if size == 'medium':
+                scale = scales.scale('image', width=600, height=600)
+            else:
+                scale = scales.scale('image', width=900, height=900)
+            if scale is not None:
+                info['url'] = scale.url
+                info['width'] = scale.width
+                info['height'] = scale.height
+            else:
+                info['url'] = IMG
+                info['width'] = '1px'
+                info['height'] = '1px'
         else:
-            item['url'] = IMG
-            item['width'] = '1px'
-            item['height'] = '1px'
-        return item
+            info['url'] = IMG
+            info['width'] = '1px'
+            info['height'] = '1px'
+        return info
 
 
 class GalleryPreview(BrowserView):
