7c272fd1a6c88df43c2104b7798ca2bf58a2dd87
Christoph Boehner <cboehner@team23.de>
2015-10-05 16:48:34 +0200

    Add view scaffolding

---
+++ src/aha.sitecontent/aha/sitecontent/browser/pagesection.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""Module providing views for a contentpage section"""
+from Acquisition import aq_inner
+from Products.Five.browser import BrowserView
+from Products.ZCatalog.interfaces import ICatalogBrain
+from plone import api
+from plone.app.contentlisting.interfaces import IContentListingObject
+from zope.component import getMultiAdapter
+
+IMG = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs='
+
+
+class PageSectionView(BrowserView):
+    """ Page Section default view """
+
+    def __call__(self):
+        self.has_assets = len(self.contained_images()) > 0
+        return self.render()
+
+    def render(self):
+        return self.index()
+
+    def contained_images(self):
+        context = aq_inner(self.context)
+        items = api.content.find(
+            context=context,
+            portal_type='Image',
+            sort_on='getObjPositionInParent'
+        )
+        return items[:9]
+
+    def image_tag(self, item):
+        data = {}
+        sizes = ['small', 'medium', 'large', 'original']
+        idx = 0
+        for size in sizes:
+            idx += 0
+            img = self._get_scaled_img(item, size)
+            data[size] = '{0} {1}w'.format(img['url'], img['width'])
+        return data
+
+    def _get_scaled_img(self, item, size):
+        if (
+                    ICatalogBrain.providedBy(item) or
+                    IContentListingObject.providedBy(item)
+        ):
+            obj = item.getObject()
+        else:
+            obj = item
+        info = {}
+        try:
+            scales = getMultiAdapter((obj, self.request), name='images')
+            if size == 'small':
+                scale = scales.scale('image', width=300, height=300)
+            if size == 'medium':
+                scale = scales.scale('image', width=600, height=600)
+            if size == 'large':
+                scale = scales.scale('image', width=900, height=900)
+            else:
+                scale = scales.scale('image', width=1200, height=1200)
+            if scale is not None:
+                info['url'] = scale.url
+                info['width'] = scale.width
+                info['height'] = scale.height
+            else:
+                info['url'] = IMG
+                info['width'] = '1px'
+                info['height'] = '1px'
+        except:
+            info['url'] = IMG
+            info['width'] = '1px'
+            info['height'] = '1px'
+        return info
