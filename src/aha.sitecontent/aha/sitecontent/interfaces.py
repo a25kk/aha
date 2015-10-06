818b4abab1980902b98ecba175ee65179012667a
Christoph Boehner <cboehner@team23.de>
2015-10-06 14:25:04 +0200

    Fix startup problems

--- src/aha.sitecontent/aha/sitecontent/interfaces.py
+++ src/aha.sitecontent/aha/sitecontent/interfaces.py
@@ -2,6 +2,7 @@
 """Module where all interfaces, events and exceptions live."""
 
 from plone.theme.interfaces import IDefaultPloneLayer
+from zope.interface import Interface
 
 
 class IAhaSitecontentLayer(IDefaultPloneLayer):
