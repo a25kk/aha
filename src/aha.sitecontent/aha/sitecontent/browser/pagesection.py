463f86a3d74423e52df3f0a65d823f5abcbd3861
Christoph Boehner <cboehner@team23.de>
2015-10-08 10:03:03 +0200

    Add browser view scaffolding

--- src/aha.sitecontent/aha/sitecontent/browser/pagesection.py
+++ src/aha.sitecontent/aha/sitecontent/browser/pagesection.py
@@ -2,12 +2,9 @@
 """Module providing views for a contentpage section"""
 from Acquisition import aq_inner
 from Products.Five.browser import BrowserView
-from Products.ZCatalog.interfaces import ICatalogBrain
-from plone import api
-from plone.app.contentlisting.interfaces import IContentListingObject
-from zope.component import getMultiAdapter
+from zope.component import getUtility
 
-IMG = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs='
+from aha.sitecontent.interfaces import IResponsiveImagesTool
 
 
 class PageSectionView(BrowserView):
@@ -20,54 +17,15 @@ class PageSectionView(BrowserView):
     def render(self):
         return self.index()
 
-    def contained_images(self):
+    def rendered_page_snippet(self):
         context = aq_inner(self.context)
-        items = api.content.find(
-            context=context,
-            portal_type='Image',
-            sort_on='getObjPositionInParent'
-        )
-        return items[:9]
+        snippet = context.restrictedTraverse('@@pagesection-snippet')
+        return snippet
 
-    def image_tag(self, item):
-        data = {}
-        sizes = ['small', 'medium', 'large', 'original']
-        idx = 0
-        for size in sizes:
-            idx += 0
-            img = self._get_scaled_img(item, size)
-            data[size] = '{0} {1}w'.format(img['url'], img['width'])
-        return data
 
-    def _get_scaled_img(self, item, size):
-        if (
-                    ICatalogBrain.providedBy(item) or
-                    IContentListingObject.providedBy(item)
-        ):
-            obj = item.getObject()
-        else:
-            obj = item
-        info = {}
-        try:
-            scales = getMultiAdapter((obj, self.request), name='images')
-            if size == 'small':
-                scale = scales.scale('image', width=300, height=300)
-            if size == 'medium':
-                scale = scales.scale('image', width=600, height=600)
-            if size == 'large':
-                scale = scales.scale('image', width=900, height=900)
-            else:
-                scale = scales.scale('image', width=1200, height=1200)
-            if scale is not None:
-                info['url'] = scale.url
-                info['width'] = scale.width
-                info['height'] = scale.height
-            else:
-                info['url'] = IMG
-                info['width'] = '1px'
-                info['height'] = '1px'
-        except:
-            info['url'] = IMG
-            info['width'] = '1px'
-            info['height'] = '1px'
-        return info
+class PageSectionSnippet(BrowserView):
+    """ Embeddable section content snippet """
+
+    def get_image_data(self, uuid):
+        tool = getUtility(IResponsiveImagesTool)
+        return tool.create(uuid)
