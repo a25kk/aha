4a5ebeff5500cfd19e2e3e792e42f90cb86911c0
Christoph Boehner <cboehner@team23.de>
2015-10-21 09:45:05 +0200

    Cleanup frontpage view

--- src/aha.sitecontent/aha/sitecontent/browser/frontpage.py
+++ src/aha.sitecontent/aha/sitecontent/browser/frontpage.py
@@ -20,39 +20,6 @@ class FrontPageView(BrowserView):
     def render(self):
         return self.index()
 
-    def image_tag(self, item):
-        data = {}
-        sizes = ['small', 'medium', 'large']
-        idx = 0
-        for size in sizes:
-            idx += 0
-            img = self._get_scaled_img(item, size)
-            data[size] = '{0} {1}w'.format(img['url'], img['width'])
-        return data
-
-    def _get_scaled_img(self, item, size):
-        if (
-            ICatalogBrain.providedBy(item) or
-            IContentListingObject.providedBy(item)
-        ):
-            obj = item.getObject()
-        else:
-            obj = item
-        info = {}
-        if hasattr(obj, 'image'):
-            scales = getMultiAdapter((obj, self.request), name='images')
-            if size == 'small':
-                scale = scales.scale('image', width=300, height=300)
-            if size == 'medium':
-                scale = scales.scale('image', width=600, height=600)
-            else:
-                scale = scales.scale('image', width=900, height=900)
-            if scale is not None:
-                info['url'] = scale.url
-                info['width'] = scale.width
-                info['height'] = scale.height
-        else:
-            info['url'] = IMG
-            info['width'] = '1px'
-            info['height'] = '1px'
-        return info
+    def get_image_data(self, uuid):
+        tool = getUtility(IResponsiveImagesTool)
+        return tool.create(uuid)
