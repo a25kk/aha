7d300425ab31a450648cf728a46bd321bac6e918
Christoph Boehner <cboehner@team23.de>
2015-10-21 11:07:20 +0200

    Fix browser view layout and integration

--- src/aha.sitecontent/aha/sitecontent/browser/contentpage.py
+++ src/aha.sitecontent/aha/sitecontent/browser/contentpage.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 """Module providing views for the folderish content page type"""
-import math
 from Acquisition import aq_inner
-from Products.Five.browser import BrowserView
-from Products.ZCatalog.interfaces import ICatalogBrain
 from plone import api
-from plone.app.contentlisting.interfaces import IContentListingObject
+from Products.Five.browser import BrowserView
 from zope.component import getMultiAdapter
-from aha.sitecontent.pagesection import IPageSection
-from aha.sitecontent.contentpage import IContentPage
+from zope.component import getUtility
 
-IMG = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs='
+from aha.sitecontent.contentpage import IContentPage
+from aha.sitecontent.interfaces import IResponsiveImagesTool
+from aha.sitecontent.pagesection import IPageSection
 
 
 class ContentPageView(BrowserView):
@@ -39,7 +37,7 @@ class ContentPageView(BrowserView):
         return template
 
 
-class GalleryPreview(BrowserView):
+class GalleryView(BrowserView):
     """Preview embeddable image gallery"""
 
     def __call__(self):
@@ -49,13 +47,20 @@ class GalleryPreview(BrowserView):
     def render(self):
         return self.index()
 
+    def contained_images(self):
+        context = aq_inner(self.context)
+        data = context.restrictedTraverse('@@folderListing')(
+            portal_type='Image',
+            sort_on='getObjPositionInParent')
+        return data
+
     def rendered_gallery(self):
         context = aq_inner(self.context)
-        template = context.restrictedTraverse('@@gallery-view')()
+        template = context.restrictedTraverse('@@gallery-snippet')()
         return template
 
 
-class GalleryView(BrowserView):
+class GallerySnippet(BrowserView):
     """Provide gallery of contained image content"""
 
     def __call__(self):
@@ -64,31 +69,6 @@ class GalleryView(BrowserView):
 
     def render(self):
         return self.index()
-
-    def has_leadimage(self):
-        context = aq_inner(self.context)
-        try:
-            lead_img = context.image
-        except AttributeError:
-            lead_img = None
-        if lead_img is not None:
-            return True
-        return False
-
-    def leadimage_tag(self):
-        context = aq_inner(self.context)
-        scales = getMultiAdapter((context, self.request), name='images')
-        scale = scales.scale('image', width=900, height=900)
-        item = {}
-        if scale is not None:
-            item['url'] = scale.url
-            item['width'] = scale.width
-            item['height'] = scale.height
-        else:
-            item['url'] = IMG
-            item['width'] = '1px'
-            item['height'] = '1px'
-        return item
 
     def contained_images(self):
         context = aq_inner(self.context)
@@ -148,48 +128,9 @@ class NewsListingView(BrowserView):
     def has_news(self):
         return len(self.news()) > 0
 
-    def image_tag(self, item):
-        data = {}
-        sizes = ['small', 'medium', 'large', 'original']
-        idx = 0
-        for size in sizes:
-            idx += 0
-            img = self._get_scaled_img(item, size)
-            data[size] = '{0} {1}w'.format(img['url'], img['width'])
-        return data
-
-    def _get_scaled_img(self, item, size):
-        if (
-                    ICatalogBrain.providedBy(item) or
-                    IContentListingObject.providedBy(item)
-        ):
-            obj = item.getObject()
-        else:
-            obj = item
-        info = {}
-        try:
-            scales = getMultiAdapter((obj, self.request), name='images')
-            if size == 'small':
-                scale = scales.scale('image', width=300, height=300)
-            if size == 'medium':
-                scale = scales.scale('image', width=600, height=600)
-            if size == 'large':
-                scale = scales.scale('image', width=900, height=900)
-            else:
-                scale = scales.scale('image', width=1200, height=1200)
-            if scale is not None:
-                info['url'] = scale.url
-                info['width'] = scale.width
-                info['height'] = scale.height
-            else:
-                info['url'] = IMG
-                info['width'] = '1px'
-                info['height'] = '1px'
-        except:
-            info['url'] = IMG
-            info['width'] = '1px'
-            info['height'] = '1px'
-        return info
+    def get_image_data(self, uuid):
+        tool = getUtility(IResponsiveImagesTool)
+        return tool.create(uuid)
 
 
 class JobListingView(BrowserView):
@@ -208,45 +149,6 @@ class JobListingView(BrowserView):
     def has_jobs(self):
         return len(self.jobs()) > 0
 
-    def image_tag(self, item):
-        data = {}
-        sizes = ['small', 'medium', 'large', 'original']
-        idx = 0
-        for size in sizes:
-            idx += 0
-            img = self._get_scaled_img(item, size)
-            data[size] = '{0} {1}w'.format(img['url'], img['width'])
-        return data
-
-    def _get_scaled_img(self, item, size):
-        if (
-                    ICatalogBrain.providedBy(item) or
-                    IContentListingObject.providedBy(item)
-        ):
-            obj = item.getObject()
-        else:
-            obj = item
-        info = {}
-        try:
-            scales = getMultiAdapter((obj, self.request), name='images')
-            if size == 'small':
-                scale = scales.scale('image', width=300, height=300)
-            if size == 'medium':
-                scale = scales.scale('image', width=600, height=600)
-            if size == 'large':
-                scale = scales.scale('image', width=900, height=900)
-            else:
-                scale = scales.scale('image', width=1200, height=1200)
-            if scale is not None:
-                info['url'] = scale.url
-                info['width'] = scale.width
-                info['height'] = scale.height
-            else:
-                info['url'] = IMG
-                info['width'] = '1px'
-                info['height'] = '1px'
-        except:
-            info['url'] = IMG
-            info['width'] = '1px'
-            info['height'] = '1px'
-        return info
+    def get_image_data(self, uuid):
+        tool = getUtility(IResponsiveImagesTool)
+        return tool.create(uuid)
